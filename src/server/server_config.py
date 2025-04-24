886dadd52b14543335a2271b052f664abb4e0464
Ricardo D. Lahuerta <ricardolahuerta@gmail.com>
2025-04-24 21:46:12 +0100

    renaming the project and pipelines

--- src/server/server_config.py
+++ src/server/server_config.py
@@ -8,12 +8,15 @@ MAX_DISPLAY_SIZE: int = 300_000
 DELETE_REPO_AFTER: int = 60 * 60  # In seconds
 
 
+# List of example repositories to display on the home page
+# Updated the first example to reflect the new name and potentially your repo URL
 EXAMPLE_REPOS: List[Dict[str, str]] = [
-    {"name": "Gitingest", "url": "https://github.com/cyclotruc/gitingest"},
+    {"name": "CodeIngest", "url": "https://github.com/Rlahuerta/CodeIngest"}, # Updated example
     {"name": "FastAPI", "url": "https://github.com/tiangolo/fastapi"},
     {"name": "Flask", "url": "https://github.com/pallets/flask"},
     {"name": "Excalidraw", "url": "https://github.com/excalidraw/excalidraw"},
     {"name": "ApiAnalytics", "url": "https://github.com/tom-draper/api-analytics"},
+    # Add or remove examples as needed
 ]
 
 templates = Jinja2Templates(directory="server/templates")
